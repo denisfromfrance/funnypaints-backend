026c845ec6a8c0615cf835a5ef0f4d3131025dda
Rukshan-Vimukthi <rukshanse.info@gmail.com>
2025-10-12 18:06:58 +0530

    New update for retrieving suits and adding itme to and retrieving items from the cart

--- User/urls.py
+++ User/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("profile/update", views.update_user_profile),
     path("profile/get-information", views.get_user_profile),
     path("profile/upload-profile-image", views.upload_user_profile_image),
+    path("cart/add", views.add_item_to_cart),
+    path("cart/items/get", views.get_items_in_cart),
 ]
